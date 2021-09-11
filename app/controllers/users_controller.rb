0d6c58b20a4aa7ea286d804e56adfd6d1933727b
crystal <noelani.villanueva@gmail.com>
2021-09-11 13:06:11 -0700

    added methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end 
+
+  def create
+    @user = User.create(user_params)
+  end 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:heartrate, :date, :symptoms, :respiratory_rate, :blood_oxygen, :active_energy )
+  end
 end
